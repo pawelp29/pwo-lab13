fcc7714bcdb93b01637df7c9be09df3542b2a7ef
Paweł Pańczyk <pawel.panczyk@pollub.edu.pl>
2022-11-19 13:31:00 +0100

    Realizacja laboratorium 13

--- src/test/java/pwo/lab13/ExcGenTest.java
+++ src/test/java/pwo/lab13/ExcGenTest.java
@@ -2,12 +2,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
  */
-package com.mycompany.lab13;
+package pwo.lab13;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
--- src/test/java/pwo/lab13/TextTest.java
+++ src/test/java/pwo/lab13/TextTest.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
  */
-package com.mycompany.lab13;
+package pwo.lab13;
 
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.params.ParameterizedTest;
